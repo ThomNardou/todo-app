2557a29a2ffafdb344b7d61dfe399cbb55e0f3c6
TiaSous <tiagorodriguessousa07@gmail.com>
2024-12-13 10:24:17 +0100

    refactor(test user): Add commentary and change expect status syntax

--- backend/tests/User/user.test.js
+++ backend/tests/User/user.test.js
@@ -1,93 +1,110 @@
 const request = require('supertest');
-const express = require('express');
+const expresponses = require('express');
 const router = require('../../routes/index.js');
 const UserModel = require('../../database/models/user.model.js');
 const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
 
-const app = express();
+const app = expresponses();
 app.use(cookieParser());
-app.use(express.json());
+app.use(expresponses.json());
 app.use(router);
 
 describe('user', () => {
+    const createUser = async () => await UserModel.create({email: 'test@example.com', password: await bcrypt.hash('test', 8)});
+    const createToken = async () => {
+        const userFound = await UserModel.findOne({ email: 'test@example.com' });
+        if (!userFound) {
+            throw new Error('User not found');
+        }
+
+        return await jwt.sign({}, require('../../env/keys/index.js'), {
+            subject: userFound._id.toString(),
+            expiresIn: 60 * 60 * 24 * 30 * 6,
+            algorithm: 'RS256',
+        })
+    }
+
+
     it('POST 200 Create user', async() => {
-        await request(app)
+        // Act
+        const response = await request(app)
             .post('/api/user/add')
             .send({ email: 'test@example.com', password: 'password123', name:"John Doe" })
-            .expect(200);
         
-            const userFound = await UserModel.findOne({ email: 'test@example.com' });
-            expect(userFound.name).toBe('John Doe');
+        const userFound = await UserModel.findOne({ email: 'test@example.com' });
+
+        // Assert
+        expect(userFound.name).toBe('John Doe');
+        expect(response.statusCode).toBe(200);
     })
 
     it('POST 400 Create user already exists', async() => {
-        await UserModel.create({email: 'test@example.com', password: await bcrypt.hash('test', 8)});
-        const res = await request(app)
+        // Arrange
+        await createUser();
+
+        // Act
+        const response = await request(app)
             .post('/api/user/add')
             .send({ email: 'test@example.com', password: 'password123', name:"John Doe" })
-            .expect(400);
-        
-        expect(res.body).toEqual('Un compte avec cet email exist déjà!');
+
+        // Assert
+        expect(response.body).toEqual('Un compte avec cet email exist déjà!');
+        expect(response.statusCode).toBe(400);
     })
 
     // À corriger dans le code code TODO
     // it('POST 400 Create user with a password with 4 char', async() => {
 
-    //     const res = await request(app)
+    //     const response = await request(app)
     //         .post('/api/user/add')
-    //         .send({ email: 'test@example.com', password: '1234', name:"John Doe" })
-    //         .expect(400);
-        
-    //         expect(res.body).toEqual('Le mot de passe fait moins de 8 caractères');
+    //         .send({ email: 'test@example.com', password: '1234', name:"John Doe" });
+
+    //         expect(response.body).toEqual('Le mot de passe fait moins de 8 caractèresponse');
+    //         expect(response.statusCode).toBe(400);
     // })
 
     it('GET 200 Get currrent user', async() => {
-        await UserModel.create({email: 'test@example.com', password: await bcrypt.hash('test', 8)});
+        // Arrange
+        await createUser();
+        const token = await createToken();
 
-        const userFound = await UserModel.findOne({ email: 'test@example.com' });
+        // Act
+        const response = await request(app).get('/api/user').set('Cookie', `token=${token}`);
 
-        if (!userFound) {
-            throw new Error('User not found');
-        }
-        const token = await jwt.sign({}, require('../../env/keys/index.js'), {
-            subject: userFound._id.toString(),
-            expiresIn: 60 * 60 * 24 * 30 * 6,
-            algorithm: 'RS256',
-        })
-        const res = await request(app).get('/api/user').set('Cookie', `token=${token}`).expect(200);
-
-        expect(res.body).toEqual({ email: 'test@example.com' });
+        // Assert
+        expect(response.body).toEqual({ email: 'test@example.com' });
+        expect(response.statusCode).toBe(200);
     })
 
     it('GET 404 User does not exist', async() => {
+        // Arrange
         const token = jwt.sign({}, require('../../env/keys/index.js'), {
             subject: "675bef360b8f6e9966770a3d",
             expiresIn: 60 * 60 * 24 * 30 * 6,
             algorithm: 'RS256',
         })
+        // Act
+        const response  = await request(app).get('/api/user').set('Cookie', `token=${token}`).expect(404);
 
-        const res  = await request(app).get('/api/user').set('Cookie', `token=${token}`).expect(404);
-        expect(res.body).toBeNull();
+        // Assert
+        expect(response.body).toBeNull();
+        expect(response.statusCode).toBe(404);
     })
 
     it('DELETE 200 delete user', async() => {
-        await UserModel.create({email: 'test@example.com', password: await bcrypt.hash('test', 8)});
+        // Arrange
+        await createUser();
+        const token = await createToken();
 
-        const userFound = await UserModel.findOne({ email: 'test@example.com' });
-
-        if (!userFound) {
-            throw new Error('User not found');
-        }
-        const token = jwt.sign({}, require('../../env/keys/index.js'), {
-            subject: userFound._id.toString(),
-            expiresIn: 60 * 60 * 24 * 30 * 6,
-            algorithm: 'RS256',
-        })
-        await request(app).delete('/api/user/delete').set('Cookie', `token=${token}`).expect(200);
+        // Act
+        const response = await request(app).delete('/api/user/delete').set('Cookie', `token=${token}`);
 
         const userNotExist = await UserModel.findOne({ email: 'test@example.com' });
+
+        // Assert
         expect(userNotExist).toBeNull();
+        expect(response.statusCode).toBe(200);
     })
 })
