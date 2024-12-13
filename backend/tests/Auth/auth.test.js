ea98aae509cbb66c498e2a94d25709a2a46c486d
TiaSous <tiagorodriguessousa07@gmail.com>
2024-12-13 10:24:02 +0100

    refactor(test auth): Add commentary and change expect status syntax

--- backend/tests/Auth/auth.test.js
+++ backend/tests/Auth/auth.test.js
@@ -9,35 +9,43 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-describe('Authentification', () => {
+describe('Authentication', () => {
+    const createUser = async () => await UserModel.create({email: 'test@example.com', password: await bcrypt.hash('test', 8)});
+
     it('POST 400 User does not exist', async() => {
+        // Act
         const response = await request(app)
             .post('/api/auth')
-            .send({ email: 'test@example.com', password: 'password123' })
-            .expect(400);
+            .send({ email: 'test@example.com', password: 'password123' });
 
+        // Assert
         expect(response.body).toEqual('Utilisateur non trouvé');
+        expect(response.statusCode).toBe(400);
     })
     it('POST 200 Connect with valid credentials', async() => {
+        // Arrange
+        await createUser();
 
-        await UserModel.create({email: 'test@example.com', password: await bcrypt.hash('test', 8)})
-
+        // Act
         const response = await request(app)
             .post('/api/auth')
-            .send({ email: 'test@example.com', password: 'test' })
-            .expect(200);
+            .send({ email: 'test@example.com', password: 'test' });
 
-        expect(response.body).toEqual({email: 'test@example.com'})
+        // Assert
+        expect(response.body).toEqual({email: 'test@example.com'});
+        expect(response.statusCode).toBe(200);
     })
     it('POST 400 Connect with not valid credentials', async() => {
+        // Arrange
+        await createUser();
 
-        await UserModel.create({email: 'test@example.com', password: await bcrypt.hash('test', 8)})
-
+        // Act
         const response = await request(app)
             .post('/api/auth')
-            .send({ email: 'test@example.com', password: 'noGoodPassword' })
-            .expect(400);
+            .send({ email: 'test@example.com', password: 'noGoodPassword' });
 
+        // Assert
         expect(response.body).toEqual('Mauvais email ou mot de passe!');
+        expect(response.statusCode).toBe(400);
     })
 })
